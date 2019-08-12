165ec5ec05a47f61e3e70d39162307ca7fb85fec
DimaProkop <dionp95@mail.ru>
2019-08-12 16:56:58 +0300

    add dice roll with images, update activity logic with `lateinit`, update build for oldest android versions, code refactoring

--- app/src/main/java/com/example/firstapp/MainActivity.kt
+++ app/src/main/java/com/example/firstapp/MainActivity.kt
@@ -1,48 +1,52 @@
 package com.example.firstapp
 
 import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var diceImageFirst: ImageView
+    lateinit var diceImageSecond: ImageView
 
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        diceImageFirst = findViewById(R.id.dice_image_1)
+        diceImageSecond = findViewById(R.id.dice_image_2)
 
         val rollButton: Button = findViewById(R.id.roll_button)
-        val countUpButton: Button = findViewById(R.id.count_button)
-        val resultText: TextView = findViewById(R.id.result_text)
 
-        rollButton.setOnClickListener { resultText.text = (randomNumber()).toString() }
-
-        countUpButton.setOnClickListener { resultText.text = countUp(resultText.text).toString() }
+        rollButton.setOnClickListener { rollDice() }
     }
 
     private fun randomNumber(): Int {
         return Random().nextInt(6) + 1
     }
 
-    private fun countUp(currentText: CharSequence): Int {
-        if (currentText.isEmpty() || currentText.isBlank() || currentText.toString().length > 1) {
-            return 1
+    private fun rollDice() {
+        val firstDice = getDiceImage()
+        diceImageFirst.setImageResource(firstDice)
+
+        var secondDice = getDiceImage()
+        while (secondDice == firstDice) {
+            secondDice = getDiceImage()
         }
-
-        var answer = Integer.parseInt(currentText.toString())
-
-        if (answer == 6) {
-            return answer
-        }
-
-        return ++answer
+        diceImageSecond.setImageResource(secondDice)
     }
 
-    private fun showToastByClick() {
-        Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+    private fun getDiceImage(): Int {
+        return  when (randomNumber()) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
     }
 }
